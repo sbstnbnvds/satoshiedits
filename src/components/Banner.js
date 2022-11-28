734fce9361bed5e97e5b1b7a0c6e9170a6757d14
sbstnbnvds <sbstnbnvdsa@gmail.com>
2022-11-28 13:04:55 -0500

    file update

---
+++ src/components/Banner.js
@@ -0,0 +1,28 @@
+import { Container, Row, Col } from "react-bootstrap";
+import headerImg from "../assets/img/header-img.svg";
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+import 'animate.css';
+import Slideshow from "./slideshow/Slideshow";
+
+export const Banner = () => {
+
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="aligh-items-center">
+          <Col xs={12} md={6} xl={7}>
+              <div>
+                <h1>Satoshi Edits</h1>
+                  <p>Satoshi Edits is the perfect solution for anyone looking to create a stand-out video. Our top quality video editing services will help improve you business video portfolio, making your company more succesful and noticed.</p>
+                  <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
+              </div>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <Slideshow/>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  )
+}
