c8615a62ade479cb01dd7e70b1a8c59f6f8b48e1
camiloers24 <camiloers24@gmail.com>
2023-01-05 17:49:08 -0500

    commit pricing (falta arreglar mejor eso)

--- src/components/pricing/Pricing.js
+++ src/components/pricing/Pricing.js
@@ -9,20 +9,25 @@ const Pricing = () => {
                     <div className="pricing-box" id="basic-plan">
                         <h3>STARTER</h3>
                         <ul>
-                            <li>Feature 1</li>
-                            <li>Feature 2</li>
-                            <li>Feature 3</li>
-                            <li>Feature 4</li>
-                            <li>Feature 5</li>
+                            <li>Cut and merge clips</li>
+                            <li>Stylish and seamless transitions</li>
+                            <li>Animated motion text</li>
+                            <li>Color correction and grading</li>
+                            <li>Background noise reduction</li>
+                            <li>Echo removal</li>
+                            <li>Sound design</li>
+                            <li>Logo reveal animation</li>
+                            <li>Slideshow</li>
+                            <li>Royalty free music</li>
                         </ul>
                         <p>19</p>
                     </div>
                 </div>
                 <div className="col-12 col-md-4 nopadding" id="diamond">
                     <div className="pricing-box" id="diamond-plan">
-                        <h3>DIAMOND</h3>
+                        <h3>ADVANCE</h3>
                         <ul>
-                            <li>Feature 1</li>
+                            <li>Starter Plan</li>
                             <li>Feature 2</li>
                             <li>Feature 3</li>
                             <li>Feature 4</li>
